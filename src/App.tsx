f8692e717b3e2f3a6a0dbda50d9da5ff63f90a61
Icaro Albar <icaro.albar@live.com>
2024-02-07 11:20:53 -0300

    :tada: First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  return (
+    <div className=" justify-center h-screen items-center flex">
+      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight text-primary first:mt-0">
+        Twygo
+      </h2>
+    </div>
+  );
+}
+
+export default App;
